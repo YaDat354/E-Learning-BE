19bb9ac89f5785939c0d2f985503273edeb3c8fd
YA DAT <22520243@gm.uit.edu.vn>
2026-04-23 13:21:55 +0700

    set up for environment for back-backend

---
+++ src/app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('E-Learning Backend Running 🚀');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
